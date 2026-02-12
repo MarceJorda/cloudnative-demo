1a156b4b02550c12cd2f0503edb3d44c18551061
Marcel <marcel.jordanaf@oxigent.io>
2026-02-12 17:50:42 +0100

    Add CI/CD pipeline

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ module.exports = app;
 
 if (require.main === module) {
   app.listen(3000, () => {
-    console.log('Server running on port 3000');
+    console.log('Server running on port 3000!!!!');
   });
 }
